f3255bbae123ff59d9e40e11ecdc708a821b08c8
Xee308 <xdyakopu@gmail.com>
2021-02-17 23:16:47 +0200

    Created Hambuger Menu
    Create hamburger menu and added the animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,7 @@
-console.log('Hello world!');
+const btnMenu = document.querySelector('#btnMenu');
+
+btnMenu.addEventListener('click', () => {
+  console.log('clicked menu');
+  btnMenu.classList.toggle('open');
+})
+
