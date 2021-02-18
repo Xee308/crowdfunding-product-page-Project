6a9b8e7ab9f455286d3f5ae29de4e1f99c780498
Xee308 <xdyakopu@gmail.com>
2021-02-18 23:31:47 +0200

    Created Poo up Menu
    Create pop up menu and overlayfor the menu, added the animation as well

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,19 @@
 const btnMenu = document.querySelector('#btnMenu');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay'); 
 
 btnMenu.addEventListener('click', () => {
   console.log('clicked menu');
-  btnMenu.classList.toggle('open');
-})
+
+  if(!header.classList.contains('open')){
+    header.classList.add('open'); 
+    overlay.classList.add('fade-in');
+    overlay.classList.remove('fade-out');
+}
+  else {
+    header.classList.remove('open');
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
+  }
+});
 
